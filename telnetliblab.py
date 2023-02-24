fa238396f1c24c4dc90736a3c953e364e205ab13
Sawadeekrub <63070020@kmitl.ac.th>
2023-02-24 21:28:19 +0700

    telnetlib 3rd push

--- telnetliblab.py
+++ telnetliblab.py
@@ -3,26 +3,28 @@ from telnetlib import Telnet
 from time import sleep
 
 host = "172.31.116.4"
-user = input("Enter username: ")
-password = getpass()
 
-tn = Telnet(host, 23, 5)
+def login(tn, credential):
+    tn.read_until(credential.encode('ascii') + b":")
+    usercred = input("Enter username: ") if credential == "Username" else getpass()
+    tn.write(usercred.encode('ascii') + b"\n")
+    sleep(1)
 
-tn.read_until(b"Username:")
-tn.write(user.encode('ascii') + b"\n")
-sleep(1)
+def setint(tn):
+    Setupcmd = ["conf t", "int g0/1", "ip address 172.31.116.17 255.255.255.240"
+            , "no sh", "do sh ip int br", "do wr", "exit", "exit", "exit"]
+    for cmd in Setupcmd:
+        tn.write((cmd + '\n').encode('ascii'))
+        sleep(2)
+    output = tn.read_very_eager()
+    print(output.decode('ascii'))
 
-tn.read_until(b"Password:")
-tn.write(password.encode('ascii') + b"\n")
-sleep(1)
+def main():
+    tn = Telnet(host, 23, 5)
+    login(tn, "Username")
+    login(tn, "Password")
+    setint(tn)
+    tn.close()
 
-Setupcmd = ["conf t", "int g0/1", "ip address 172.31.116.17 255.255.255.240"
-        , "no sh", "do sh ip int br", "do wr", "exit", "exit", "exit"]
-for cmd in Setupcmd:
-    tn.write((cmd + '\n').encode('ascii'))
-    sleep(2)
-
-output = tn.read_very_eager()
-print(output.decode('ascii'))
-
-tn.close()
+if __name__ == "__main__":
+    main()
